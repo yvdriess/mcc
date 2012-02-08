4a72270c0a1c8fa227d0d08cd27412616a3d0b32
Yves Vandriessche <yvdriess@vub.ac.be>
2012-02-08 14:13:48 +0100

    take 4

--- stresstest.lisp
+++ stresstest.lisp
@@ -2,7 +2,7 @@
   (defvar *indentation* 0)
   (defvar *seperator* "")
   (defvar *line-stream* t)
-  (defvar *filename* "mccompiled")
+  (defvar *filename* "stresstest")
 
   (defun symbol-to-string (sym)
     (string-downcase (symbol-name sym)))
@@ -75,34 +75,158 @@ struct context;
     (line "int execute( const int& t, context& c ) const;"))
   (line "};"))
 
-(defun generate-context-header (item-names tag-names prescriptions tuned-steps)
+(defun generate-context-header (item-names tag-names prescriptions)
   (line "~%struct context: public CnC::context< context > {~%")
   (indented
-    (lines "CnC::item_collection< int, amplitude > ~A; // SIZE=~A" item-names)
+    (lines "CnC::item_collection< int, int > ~A;" item-names)
     (line "CnC::item_collection< int, bool > signals;")
     (lines "CnC::tag_collection< int > ~A;" tag-names)
     (line "context(): ")
     (indented 
       (line "CnC::context< context >(),")
       (lines "~A( this )," item-names)
-      (line "signals( this ),")
       (comma-seperated
 	(lines "~A( this , false )" tag-names))
       (indented
 	(line "{")
 	(indented
 	  (loop for entry in prescriptions
-	     for tuned = (assoc (car entry) tuned-steps)
-	     if tuned
-							   ; this is
-							   ; hidious!
-	       do (line "prescribe( ~A , ~A() , ~A(~{ ~A ~^,~}) );" 
-			(cdr entry) (car entry) (cadr tuned) (cddr tuned))
-	     else do (line "prescribe( ~A , ~A() );" (cdr entry) (car entry)))
-	  #+nil(lines "prescribe( ~A, ~A() );"
-		 (mapcar #'cdr prescriptions)
-		 (mapcar #'car prescriptions))
-;	  (lines "~A.put(0);" input-tag-names)
+		do (line "prescribe( ~A , ~A() );" (cdr entry) (car entry)))
 	  )
 	(line "}~%"))))
   (line "};~%"))
+
+
+(defun generate-main-source (input-tag-names)
+  (line "int main(int argc, char* argv[]) {")
+  (indented
+    (line "const int elements = argc>1 ? argv[1] : 32")
+    (line "context ctx;")
+    ;; insert code that fills the right tag and item collections with elements
+    (lines "ctx.~A.put(elements);" input-tag-names)
+
+    (line "ctx.wait();")
+    ;; insert code that retrieves the info
+    (line "return CnC::CNC_Success;"))
+  (line "}~%"))
+
+(defun generate-step-source (step-name step-body)
+  (line "int ~A::execute(const int & t, context & c ) const {" step-name)
+  ;; insert step logic here
+  (indented 
+    (line step-body)
+    (line "return CnC::CNC_Success;"))
+  (line "}~%"))
+
+(defun generate-source (step-names step-bodies input-tag-names)
+  (line "#include <stdio.h>")
+  (line "#include <stdlib.h>")
+  (line "#include \"~A.h\"~%" *filename*)
+  (generate-main-source input-tag-names)
+  (mapcar #'generate-step-source step-names step-bodies)
+  )
+
+
+(defmacro write-to-file (pathname &body body)
+  "Writes all 'line' and 'lines' called in the body to the specified file path."
+  `(with-open-file (out ,pathname
+			:direction :output 
+			:if-does-not-exist :create 
+			:if-exists :supersede)
+     (let ((*line-stream* out))
+       ,@body)))
+
+(defmacro writing-to-string (&body body)
+  `(let ((*line-stream* (make-string-output-stream)))
+     ,@body
+     (get-output-stream-string *line-stream*)))
+
+(defun generate-header (item-names 
+			tag-names 
+			step-names
+			prescriptions
+)
+  ;; preamble
+  (line #.*header*)
+  (mapc #'generate-step-header step-names)
+  (newline)
+  (generate-context-header item-names tag-names prescriptions)
+  (line "#endif"))
+
+(defun build (item-names tag-names step-names step-bodies 
+	      input-tag-names prescriptions
+	      &key (target-directory "") 
+	           (target-header-file "stresstest.h") 
+	           (target-source-file "stresstest.C"))
+  (let ((header (concatenate 'string target-directory target-header-file))
+	(source (concatenate 'string target-directory target-source-file)))
+    (format t "Generating header file... ")
+    (write-to-file header
+      (generate-header item-names 
+		       tag-names 
+		       step-names
+		       prescriptions))
+    (format t "done~%Generating source file... ")
+    (write-to-file source
+      (generate-source step-names step-bodies input-tag-names))
+    (format t "done~%Written to ~A and ~A.~%" header source)))
+
+(defun step-body (n)
+  (format nil "
+int i = t;
+int data;
+c.items_~d.get(i, data);
+c.items_~d.put(i, ++data);
+c.tags_~d.put(i);
+" 
+	  n (1+ n) (1+ n))
+)
+
+(defun source-body (size)
+  (format nil "
+for(int i=0;i<~d; ++i) {
+  c.items_1.put(i,1);
+  c.tags_1.put(i);
+}
+"
+	  size))
+
+(defun sink-body (last size)
+  (format nil "
+int results[~d];
+for(int i=0;i<~d; ++i) {
+  c.items_~d.get(i,results[i]);
+  printf(\"results[%d]: %d\\n\",i,results[i]);
+}
+"
+	  size
+	  size
+	  last))
+
+(defun generate-depth-stresstest (depth)
+  (let ((size 16)
+	(item-names    (loop for i from 1 to depth
+			     collect (format nil "items_~d" i)))
+	(tag-names     (loop for i from 1 to depth
+			     collect (format nil "tags_~d" i)))
+	(step-names    (loop for i from 1 below depth
+			     collect (format nil "step_~d" i)))
+	(step-bodies   (loop for i from 1 below depth
+			     collect (step-body i)))
+	(prescriptions (loop for i from 1 below depth
+			     collect (cons (format nil "step_~d" i)
+					   (format nil "tags_~d" i)))))
+    (build item-names
+	   tag-names
+	   (append step-names
+		   (list "source" "sink"))
+	   (append step-bodies
+		   (list (source-body size)
+			 (sink-body depth size)))
+	   (list "source")
+	   (append prescriptions
+		   (list (cons "sink" 
+			       (format nil "tags_~d" depth)))))
+))
+
+(generate-depth-stresstest 3)
